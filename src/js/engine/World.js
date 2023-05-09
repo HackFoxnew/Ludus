97c85cbd571017a20a4c019b8cd0479297399620
DFox <100820447+HackFoxnew@users.noreply.github.com>
2023-05-09 18:38:48 -0400

    Actualizar World.js
    Class Exprite add

--- src/js/engine/World.js
+++ src/js/engine/World.js
@@ -45,3 +45,78 @@ class World {
 }
 
 
+
+class MiSprite extends PIXI.Sprite {
+  constructor(texture) {
+    super(texture);
+    // Añadimos los atributos velocity y acceleration
+    this.velocity = {x: 0, y: 0};
+    this.acceleration = {x: 0, y: 0};
+  }
+
+  // Definimos el método setVelocity que recibe x y y como parámetro
+  setVelocity(x, y) {
+    // Asignamos los valores de los parámetros a los atributos velocity
+    this.velocity.x = x;
+    this.velocity.y = y;
+  }
+
+  // Definimos el método setAcceleration que recibe x y y como parámetro
+  setAcceleration(x, y) {
+    // Asignamos los valores de los parámetros a los atributos acceleration
+    this.acceleration.x = x;
+    this.acceleration.y = y;
+  }
+
+  // Definimos el método setSize que recibe width y height como parámetro
+  setSize(width, height) {
+    // Asignamos los valores de los parámetros a los atributos width y height
+    this.width = width;
+    this.height = height;
+  }
+
+  // Definimos el método setRotation que recibe angle como parámetro
+  setRotation(angle) {
+    // Asignamos el valor del parámetro al atributo rotation
+    this.rotation = angle;
+  }
+
+  // Definimos el método setPosition que recibe x y y como parámetro
+  setPosition(x, y) {
+    // Asignamos los valores de los parámetros a los atributos x e y
+    this.x = x;
+    this.y = y;
+  }
+
+  // Definimos el método setAnchor que recibe x y y como parámetro
+  setAnchor(x, y) {
+    // Asignamos los valores de los parámetros al atributo anchor
+    this.anchor.set(x, y);
+  }
+
+  // Definimos el método setTint que recibe color como parámetro
+  setTint(color) {
+    // Asignamos el valor del parámetro al atributo tint
+    this.tint = color;
+  }
+
+  // Definimos el método setBlendMode que recibe mode como parámetro
+  setBlendMode(mode) {
+    // Asignamos el valor del parámetro al atributo blendMode
+    this.blendMode = mode;
+  }
+
+  // Modificamos el método update para que el sprite se mueva según su velocidad y aceleración
+  update(delta) {
+    // Actualizamos la posición del sprite sumando la velocidad multiplicada por el delta
+    this.x += this.velocity.x * delta;
+    this.y += this.velocity.y * delta;
+    // Actualizamos la velocidad del sprite sumando la aceleración multiplicada por el delta
+    this.velocity.x += this.acceleration.x * delta;
+    this.velocity.y += this.acceleration.y * delta;
+  }
+}
+
+
+
+
