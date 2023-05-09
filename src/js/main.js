aa4b1314cc5db1bee59284327c05bf5c75eeb205
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-05-09 00:07:20 -0400

    added pixi renderer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 
+let renderer;
 document.addEventListener("DOMContentLoaded", onStart);
 
 
@@ -11,6 +12,13 @@ function onStart () {
     //libs status
     console.log("*Eruda " + eruda.version + " ok!");
     console.log("*PixiJS " + PIXI.VERSION + " ok!");
-
+    
+    // create game renderer
+    renderer = new PIXI.Renderer({
+        width: canvasWidth,
+        height: canvasHeight,
+        backgroundColor: 0x00ccff,
+        view: document.getElementById("game-renderer"),
+    });
     
 }
