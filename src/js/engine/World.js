f66c59d9ec4fbbb2de3a717ef74a9062935fbd7d
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-05-09 01:06:50 -0400

    update World.js

--- src/js/engine/World.js
+++ src/js/engine/World.js
@@ -1,35 +1,47 @@
 
 class World {
-  // El constructor toma por parámetro el ancho y el alto del mundo
-  constructor(width, height) {
-    // Creamos el contenedor de pixi
-    this.container = new PIXI.Container();
-    // Creamos el ticker de pixi
-    this.ticker = new PIXI.Ticker();
-    // Ajustamos el tamaño del contenedor al ancho y alto del mundo
-    this.container.width = width;
-    this.container.height = height;
-  }
+    
+    constructor(width, height) {
+        // Creamos el contenedor y ticker de pixi
+        const container = new PIXI.Container();
+        const layer = new PIXI.Container();
+        const ticker = new PIXI.Ticker();
+        
+        // Ajustamos el tamaño del contenedor al ancho y alto del mundo
+        container.width = width;
+        container.height = height;
+        container.addChild(layer);
+        
+        // Añadir tick
+        ticker.add(delay => {
+            this.update(delta);
+            renderer.render(container);
+        });
+        
+        this.container = container;
+        this.layer = layer;
+        this.ticker = ticker;
+    }
 
-  // Un método para añadir objetos al contenedor
-  add(object) {
-    this.container.addChild(object);
-  }
+    // Un método para añadir objetos al contenedor
+    add(object) {
+        this.layer.addChild(object);
+    }
 
-  // Un método para iniciar el ticker
-  start() {
-    this.ticker.start();
-  }
+    // Un método para iniciar el ticker
+    start() {
+        this.ticker.start();
+    }
 
-  // Un método para detener el ticker
-  stop() {
-    this.ticker.stop();
-  }
+    // Un método para detener el ticker
+    stop() {
+        this.ticker.stop();
+    }
 
-  // Un método para actualizar el mundo en cada tick
-  update(delta) {
-    // Logica
-  }
+    // Un método para actualizar el mundo en cada tick
+    update(delta) {
+        // Logica
+    }
 }
 
 
